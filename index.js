a6548890c30b6ff051b690da326e52fa9656e5c6
Rhys Gordon <RGordon2614@gmail.com>
2025-11-03 13:03:31 +0000

    updated error handler, added JOI schema validations

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const ejsMate = require("ejs-mate");
 const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
+const Joi = require("joi");
 const Campground = require("./models/campground");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
@@ -42,6 +43,24 @@ app.get("/campgrounds/new", (req, res) => {
 app.post(
   "/campgrounds",
   catchAsync(async (req, res, next) => {
+    // if (!req.body.campground) {
+    //   throw new ExpressError("Invalid Campground Data", 400);
+    // }
+
+    const campgroundSchema = Joi.object({
+      campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        desription: Joi.string().required(),
+        location: Joi.string().required(),
+      }).required(),
+    });
+    const result = campgroundSchema.validate(req.body);
+    if (result.error) {
+      const msg = result.error.details.map((el) => el.message).join(",");
+      throw new ExpressError(msg, 400);
+    }
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -84,8 +103,15 @@ app.delete(
   })
 );
 
+// For every single request
+app.all(/(.*)/, (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
+
 app.use((err, req, res, next) => {
-  res.send("Oh no, something went wrong!");
+  const { statusCode = 500 } = err;
+  if (!err.message) err.message = "Oh no, something went wrong!";
+  res.status(statusCode).render("error", { err });
 });
 
 app.listen(3000, () => {
