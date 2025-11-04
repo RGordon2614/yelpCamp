bf1eefc7d896437c840c3d4f64d0808b8838462f
Rhys Gordon <RGordon2614@gmail.com>
2025-11-04 21:29:00 +0000

    added delete functionality to reviews

--- index.js
+++ index.js
@@ -78,7 +78,6 @@ app.get(
     const campground = await Campground.findById(req.params.id).populate(
       "reviews"
     );
-    console.log(campground);
     res.render("campgrounds/show", { campground });
   })
 );
@@ -122,6 +121,16 @@ app.post(
     await review.save();
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+app.delete(
+  "/campgrounds/:id/reviews/:reviewId",
+  catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
   })
 );
 
