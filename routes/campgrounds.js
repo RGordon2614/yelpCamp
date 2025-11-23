65db2d602b234a9ac91ec729cdedc47a940da252
Rhys Gordon <RGordon2614@gmail.com>
2025-11-23 14:27:57 +0000

    populate and display author of each review. added authorization for deleting reviews

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -36,8 +36,14 @@ router.get(
   "/:id",
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
-      .populate("reviews")
+      .populate({
+        path: "reviews",
+        populate: {
+          path: "author",
+        },
+      })
       .populate("author");
+    console.log(campground.reviews);
     if (!campground) {
       req.flash("error", "Campground not found!");
       return res.redirect("/campgrounds");
--- routes/reviews.js
+++ routes/reviews.js
@@ -7,7 +7,7 @@ const ExpressError = require("../utils/ExpressError");
 
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-const { validateReview, isLoggedIn } = require("../middleware");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 
 router.post(
   "/",
@@ -27,6 +27,8 @@ router.post(
 
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
