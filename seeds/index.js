d1bf33abfe3c188b8ceef33cd3ce0835c385304d
Rhys Gordon <RGordon2614@gmail.com>
2025-11-26 13:45:30 +0000

    Added image carousel, Reseeded database with new images

--- seeds/index.js
+++ seeds/index.js
@@ -24,7 +24,16 @@ const seedDB = async () => {
       author: "69133a22698cf97b181d047b",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: `https://picsum.photos/400?random=${Math.random()}`,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dyehvvzca/image/upload/v1764079387/YelpCamp/isaytuq04o4ixy1jphho.jpg",
+          filename: "YelpCamp/isaytuq04o4ixy1jphho",
+        },
+        {
+          url: "https://res.cloudinary.com/dyehvvzca/image/upload/v1764079388/YelpCamp/dalypxtrp0bthmotznzo.jpg",
+          filename: "YelpCamp/dalypxtrp0bthmotznzo",
+        },
+      ],
       description:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam non interdum sem, eu viverra tortor. Ut ullamcorper nec tellus id pharetra. Pellentesque sed suscipit ipsum. Curabitur ac blandit ex. Pellentesque at felis et leo ultricies aliquam eu sit amet ipsum. Integer quis tortor vehicula, eleifend leo sit amet, accumsan lectus. ",
       price,
